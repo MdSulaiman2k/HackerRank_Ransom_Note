ffc675daa7c1657ddc80c8b1e7e6e285167d7307
md Sulaiman <77278345+MdSulaiman2k@users.noreply.github.com>
2021-01-26 15:24:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Main {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+        boolean result  = true ;
+        if(note.length > magazine.length){
+             System.out.println("No");
+             return ;
+             }
+        for(String a : note){
+            boolean check = true ;
+            for(int i = 0 ; i < magazine.length && check  ; i++){
+                  if(a.equals(magazine[i])){
+                      check = false ;
+                      magazine[i] = "" ;
+                  }
+            }
+            if(check){
+                result = false ; 
+                break ;
+            }
+        }
+        
+       if(result )
+          System.out.println("Yes") ;
+        else 
+          System.out.println("No") ;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
